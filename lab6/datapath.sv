ac6cf10982e16e0c31e69e18862bfc365d4f394c
yaowentao001 <15726938186@163.com>
2021-10-26 21:02:12 +0800

    f

--- lab6/datapath.sv
+++ lab6/datapath.sv
@@ -11,15 +11,15 @@ module datapath(
 ); // TODO:input ? output? 
 
 logic[15:0] BUS;
-logic[15:0] MDR_input,MDR_mux,MDR_output, MAR_output, ALU_output, PC_output, MARMUX_output,IR_output; 
+logic[15:0] MDR_input,MDR_output, MAR_output, ALU_output, PC_output, MARMUX_output,IR_output; 
 
 
-reg_parallel_16 MDR_unit(.Clk(Clk),.Load(LD_MDR),.D(MDR_mux),.Data_Out(MDR_output));
+reg_parallel_16 MDR_unit(.Clk(Clk),.Load(LD_MDR),.D(MDR_input),.Data_Out(MDR_output));
 reg_parallel_16 MAR_unit(.Clk(Clk),.Load(LD_MAR),.D(BUS),.Data_Out(MAR_output));
 reg_parallel_16 IR_unit(.Clk(Clk),.Load(LD_IR),.D(BUS),.Data_Out(IR_output));
 
 BUS_select bus_select(.MDR2BUS(MDR_output), .ALU2BUS(ALU_output),.PC2BUS(PC_output),.MARMUX2BUS(MARMUX_output),.GateMDR(GateMDR),.GateALU(GateALU),.GatePC(GatePC),.GateMARMUX(GateMARMUX),.BUS(BUS));
-PC_module PC_unit(	.Clk(Clk), .LD_PC(LD_PC),.PCMUX(PCMUX),.Data_from_BUS(BUS),.Data_from_addrmux_to_PC(),.DataOut(PC_output));
+PC_module PC_unit(	.Clk(Clk), .LD_PC(LD_PC),.PCMUX(PCMUX),.Data_from_BUS(BUS),.Data_from_addrmux_to_PC(),.DataOut(PC_output),.reset(Reset_ah));
 assign MDR = MDR_output;
 assign MAR = MAR_output;
 assign PC = PC_output;
@@ -30,9 +30,9 @@ assign IR = IR_output;
 
 always_comb begin
     if(MIO_EN)
-        MDR_input = BUS;
-    else
         MDR_input = MDR_In;
+    else
+        MDR_input = BUS;
 end
 endmodule
 
@@ -124,7 +124,7 @@ module counter16bit (
 			Dout[0] <= Din[0] ^ 1'b1;
 			Dout[1] <= Din[1] ^ Din[0];
 			Dout[2] <= Din[1] & Din[0] ^ Din[2];
-			Dout[6] <= Din[0]& Din[1]& Din[2]^ Din[3];
+			Dout[3] <= Din[0]& Din[1]& Din[2]^ Din[3];
 			Dout[4] <= Din[0]& Din[1]& Din[2]& Din[3]^ Din[4];
 			Dout[5] <= Din[0]& Din[1]& Din[2]& Din[3]& Din[4]^ Din[5];
 			Dout[6] <= Din[0]& Din[1]& Din[2]& Din[3]& Din[4]& Din[5]^ Din[6];
@@ -146,19 +146,29 @@ module counter16bit (
 module PC_module (
 		input logic Clk,
 		input logic LD_PC,
+		input logic reset,
 		input logic [1:0] PCMUX,
 		input logic [15:0] Data_from_BUS,
 		input logic [15:0] Data_from_addrmux_to_PC,
 		output logic [15:0] DataOut
 );
 
-	logic [15:0] Data_from_PCMUX, PCplus1;	
+	logic [15:0] Data_from_PCMUX, PCplus1,dataout_mid;	
 	
-	reg_parallel_16 REG_PC(.Clk(Clk), .Load(LD_PC), .D(Data_from_PCMUX), .Data_Out(DataOut));
+	reg_parallel_16 REG_PC(.Clk(Clk), .Load(LD_PC | reset), .D(Data_from_PCMUX), .Data_Out(DataOut));
 	
-	mux4to1bit16 PCmultiplexer(.Din1(PCplus1), .Din2(Data_from_addrmux_to_PC), .Din3(Data_from_BUS),.Din4(),.select(PCMUX), .Dout(Data_from_PCMUX));
+	mux4to1bit16 PCmultiplexer(.Din1(PCplus1), .Din2(Data_from_BUS), .Din3(Data_from_addrmux_to_PC),.Din4(),.select(PCMUX), .Dout(dataout_mid));
 	
 	counter16bit counter(.Clk(Clk),.Din(DataOut), .Dout(PCplus1));
+	
+	always_comb
+		begin
+			if(reset)
+				Data_from_PCMUX = 16'b0;
+			else
+				Data_from_PCMUX = dataout_mid;
+		end
+		
 	
 endmodule
 	
