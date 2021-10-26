2a0bb01a0ff5dee1d436853d5935b41c4f666a33
yaowentao001 <15726938186@163.com>
2021-10-26 16:04:13 +0800

    g

--- lab6/datapath.sv
+++ lab6/datapath.sv
@@ -6,24 +6,46 @@ input logic GatePC, GateMDR, GateALU,GateMARMUX,
 input logic[1:0] PCMUX,ADDR2MUX,ALUK,
 input logic DRMUX, SR1MUX, SR2MUX, ADDR1MUX,
 input logic MIO_EN,
-input logic[15:0] MDR_In, MAR, MDR, IR, PC,Data_from_SRAM, Data_to_SRAM
+input logic[15:0] MDR_In, MDR, IR, PC,Data_from_SRAM, Data_to_SRAM, 
+output logic[15:0] MAR
 ); // TODO:input ? output? 
 
 logic[15:0] BUS;
-
-logic[15:0] MDR_input;
-
-always_ff @(posedge Clk) begin
-
-end
-
+logic[15:0] MDR_input,MDR_mux,MDR_output, MAR_output, ALU_output, PC_output, MARMUX_output;
+reg_parallel_16 MDR_unit(.Clk(Clk),.Load(LD_MDR),.D(MDR_mux),.Data_Out(MDR_output));
+reg_parallel_16 MAR_unit(.Clk(Clk),.Load(LD_MAR),.D(BUS),.Data_Out(MAR_output));
+BUS_select bus_select(.MDR2BUS(MDR_output), .ALU2BUS(ALU_output),.PC2BUS(PC_output),.MARMUX2BUS(MARMUX_output),.GateMDR(GateMDR),.GateALU(GateALU),.GatePC(GatePC),.GateMARMUX(GateMARMUX));
+assign MDR = MDR_output;
+assign MAR = MAR_output;
 
 
 always_comb begin
     if(MIO_EN)
-
+        MDR_input = BUS;
+    else
+        MDR_input = MDR_In;
 end
 
+endmodule
+
+
+
+
+module BUS_select(
+    input logic[15:0] MDR2BUS, ALU2BUS, PC2BUS, MARMUX2BUS,
+    input logic GateMDR, GateALU, GatePC, GateMARMUX,
+    output logic[15:0] BUS
+);
+    always_comb begin
+        if(GateMDR)
+            BUS = MDR2BUS;
+        else if (GateALU)
+            BUS = ALU2BUS;
+        else if (GatePC)
+            BUS = PC2BUS;
+        else if (GateMARMUX)
+            BUS = MARMUX2BUS;       
+    end
 endmodule
 
 module reg_parallel_16 (
@@ -34,9 +56,9 @@ module reg_parallel_16 (
     always_ff @ (posedge Clk)
     begin
 		 if (Load)
-		 
-			  Data_Out <= D;
-		 
+			  Data_Out <= D; 
+         else 
+            Data_Out <= Data_Out;		 
     end
 	 
 endmodule
