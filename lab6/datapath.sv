324cd7e4be3c7f151f2f25551c13f5476087c2dd
HirachaleFranklin <Zitai.19@intl.zju.edu.cn>
2021-10-26 15:57:05 +0800

    10261

--- lab6/datapath.sv
+++ lab6/datapath.sv
@@ -30,5 +30,44 @@ module reg_parallel_16 (
 	 
 endmodule
 
+module mux4to1bit16 (
+		input [15:0] Din1,
+		input [15:0] Din2,
+		input [15:0] Din3,
+		input [15:0] Din4,
+		input [3:0] select,
+		output logic [15:0] Dout);
+	
+	always_comb
+		begin
+			if (select == 2'b00)
+				Dout = Din1;
+			else if (select == 2'b01)
+				Dout = Din2;
+			else if (select == 2'b10)
+				Dout = Din3;
+			else if (select == 2'b11)
+				Dout = Din4;
+		end
+endmodule
 
-module PC 
+
+module PC (
+		input logic Clk,
+		input logic LD_PC,
+		input logic GatePC,
+		input logic [1:0] PCMUX,
+		input logic [15:0] Data_from_BUS,
+		input logic [15:0] Data_from_addrmux_to_PC,
+		output logic [15:0] Data_to_BUS,
+		output logic [15:0] Data_to_MARMUX
+);
+	logic [15:0] Data_from_PCMUX;	
+	reg_parallel_16 REG_PC(.Clk(Clk), .Load(LD_PC), .D(Data_from_PCMUX), .Data_Out(Data_from_addrmux_to_PC));
+	
+	
+	
+	
+	
+	
+	
