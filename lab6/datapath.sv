3521bcea961f2366565314dd401b6d95c26b390b
yaowentao001 <15726938186@163.com>
2021-11-02 21:27:06 +0800

    comment

--- lab6/datapath.sv
+++ lab6/datapath.sv
@@ -8,7 +8,7 @@ module datapath(
 	input logic MIO_EN,
 	input logic[15:0] MDR_In,
 	output logic[15:0] MAR,IR,MDR, PC
-); // TODO:input ? output? 
+); 	//TODO: Clearify the pins 
 
 logic[15:0] BUS;
 logic[15:0] MDR_input,MDR_output, MAR_output, ALU_output, PC_output, MARMUX_output,IR_output,IR_input; 
@@ -60,86 +60,7 @@ module BUS_select(
     end
 endmodule
 
-module reg_parallel_16 (
-				  input  logic Clk, Load,
-              input  logic [15:0]  D,
-              output logic [15:0]  Data_Out);
 
-    always_ff @ (posedge Clk)
-    begin
-		 if (Load)
-			  Data_Out <= D; 
-         else 
-            Data_Out <= Data_Out;		 
-    end
-	 
-endmodule
-
-module mux4to1bit16 (
-		input [15:0] Din1,
-		input [15:0] Din2,
-		input [15:0] Din3,
-		input [15:0] Din4,
-		input [1:0] select,
-		output logic [15:0] Dout);
-	
-	always_comb
-		begin
-			if (select == 2'b00)
-				Dout = Din1;
-			else if (select == 2'b01)
-				Dout = Din2;
-			else if (select == 2'b10)
-				Dout = Din3;
-			else
-				Dout = Din4;
-		end
-endmodule
-
-
-
-module mux2to1bit16 (
-		input [15:0] Din1,
-		input [15:0] Din2,
-		input select,
-		output logic [15:0] Dout);
-	
-	always_comb
-		begin
-			if (select == 1'b0)
-				Dout = Din1;
-			else
-				Dout = Din2;
-		end
-endmodule
-
-
-
-module counter16bit (
-		input [15:0] Din,
-		input logic Clk,
-		output logic [15:0] Dout);
-		
-	always_ff @ (posedge Clk)
-		begin
-			Dout[0] <= Din[0] ^ 1'b1;
-			Dout[1] <= Din[1] ^ Din[0];
-			Dout[2] <= Din[1] & Din[0] ^ Din[2];
-			Dout[3] <= Din[0]& Din[1]& Din[2]^ Din[3];
-			Dout[4] <= Din[0]& Din[1]& Din[2]& Din[3]^ Din[4];
-			Dout[5] <= Din[0]& Din[1]& Din[2]& Din[3]& Din[4]^ Din[5];
-			Dout[6] <= Din[0]& Din[1]& Din[2]& Din[3]& Din[4]& Din[5]^ Din[6];
-			Dout[7] <= Din[0]& Din[1]& Din[2]& Din[3]& Din[4]& Din[5]& Din[6]^ Din[7];
-			Dout[8] <= Din[0]& Din[1]& Din[2]& Din[3]& Din[4]& Din[5]& Din[6]& Din[7]^ Din[8];
-			Dout[9] <= Din[0]& Din[1]& Din[2]& Din[3]& Din[4]& Din[5]& Din[6]& Din[7]& Din[8]^ Din[9];
-			Dout[10] <= Din[0]& Din[1]& Din[2]& Din[3]& Din[4]& Din[5]& Din[6]& Din[7]& Din[8]& Din[9]^ Din[10];
-			Dout[11] <= Din[0]& Din[1]& Din[2]& Din[3]& Din[4]& Din[5]& Din[6]& Din[7]& Din[8]& Din[9]& Din[10]^ Din[11];
-			Dout[12] <= Din[0]& Din[1]& Din[2]& Din[3]& Din[4]& Din[5]& Din[6]& Din[7]& Din[8]& Din[9]& Din[10]& Din[11]^ Din[12];
-			Dout[13] <= Din[0]& Din[1]& Din[2]& Din[3]& Din[4]& Din[5]& Din[6]& Din[7]& Din[8]& Din[9]& Din[10]& Din[11]& Din[12]^ Din[13];
-			Dout[14] <= Din[0]& Din[1]& Din[2]& Din[3]& Din[4]& Din[5]& Din[6]& Din[7]& Din[8]& Din[9]& Din[10]& Din[11]& Din[12]& Din[13]^ Din[14];
-			Dout[15] <= Din[0]& Din[1]& Din[2]& Din[3]& Din[4]& Din[5]& Din[6]& Din[7]& Din[8]& Din[9]& Din[10]& Din[11]& Din[12]& Din[13]& Din[14] ^ Din[15];
-		end
-	endmodule
 			
 
 	
