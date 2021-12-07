2f8fa01ae493728963c75a01a711659400702185
HirachaleFranklin <Zitai.19@intl.zju.edu.cn>
2021-12-07 15:17:32 +0800

    a

--- lab9/AES.sv
+++ lab9/AES.sv
@@ -17,8 +17,14 @@ module AES (
 	output logic [127:0] AES_MSG_DEC
 );
 
-
-
+	logic [1407:0] KeySchedule;
+	logic [127:0] state;
+	logic [127:0] key;
+	logic [127:0] addroundkey_out;
+	logic [127:0] invshiftrows_in;
+	logic [127:0] invshiftrows_out;
+	logic [127:0] mixcolumns_in;
+	logic [127:0] mixcolumns_out;
 
 	enum logic [4:0]{
 		WAIT, 
@@ -41,7 +47,7 @@ module AES (
 		INVMIXCOL4,
 		
 		INVSHIFTROW,
-		LOOPADDRK,
+		ADDRK
 
 
 
@@ -60,10 +66,59 @@ module AES (
 		end
 		
 		else begin
-			state <= next_state;
 			AES_STATE <= AES_NEXT_STATE;
 			Loop_counter <= Loop_counter_next;
 		end
 	end
+	
+	always_comb
+	begin
+	
+	//Transition relation
+	
+	end
+	
+	always_comb
+	begin
+		
+		unique case (AES_STATE)
+			
+			WAIT:
+				begin
+					AES_DONE = 0;
+				end
+				
+			DONE:
+				begin
+					AES_DONE = 1;
+				end
+			
+			KEY_EXPANSION:
+				begin
+					
+					AES_DONE = 0;
+				end
+				
+			INITIAL_ROUND:
+				begin
 
+					AES_DONE = 0;
+				end
+				
+		endcase
+		
+	end
+	
+	
+	//KeyExpansion
+	KeyExpansion keyexpansion(.clk(CLK), .Cipherkey(AES_KEY), .KeySchedule(KeySchedule));
+
+	// AddRoundKey
+	AddRoundKey addroundkey(.state(state), .roundKey(key), .out(addroundkey_out));
+	
+	//InvShiftRows
+	InvShiftRows invshiftrows(.data_in(invshiftrows_in), .data_out(invshiftrows_out));
+
+	// InvMixColumns
+	InvMixColumns invmixcolumns(.in(mixcolumns_in),.out(mixcolumns_out));
 endmodule
